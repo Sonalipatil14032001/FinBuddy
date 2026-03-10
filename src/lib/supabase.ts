434fe81dcd0bf3e8f613db5bc18d7713e4b98395
Sonalipatil14032001 <sonalispatil14032001@gmail.com>
2026-03-09 19:05:19 -0500

    env variables

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://ruaptqoqzcvmrumsajlp.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ1YXB0cW9xemN2bXJ1bXNhamxwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI2NTU0OTcsImV4cCI6MjA4ODIzMTQ5N30.ivd0I4fPRCC4HpGgI8U-lf1zKQl0nuN6cFFCsbfJX1o'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
 
